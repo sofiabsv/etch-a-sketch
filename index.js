3e0250d16b6693a62410f392adf5574e63d7b25a
Sofia Berdichevska <sofiaxberdichevska@gmail.com>
2024-06-12 17:59:45 +0200

    Refactored code to allow Creation of new grid

--- index.js
+++ index.js
@@ -1,17 +1,38 @@
-const container = document.querySelector('.container');
+const container = document.querySelector(".container");
 
-for(let i = 0; i < 16; i++){
-  for(let j = 0; j < 16; j++){
-    const square = document.createElement('div');
-    square.classList.add('square');
-    container.appendChild(square);
-
-    square.addEventListener('mouseover', () => {
-  square.style.backgroundColor = 'black';
-});
-
-  }
+function clearGrid() {
+  container.innerHTML = "";
 }
 
+createNewGrid = (num) => {
+  clearGrid();
+  const borderWidth = 1;
+  const squareSize = (704 - num * borderWidth * 2) / num;
 
+  for (let i = 0; i < num; i++) {
+    for (let j = 0; j < num; j++) {
+      const square = document.createElement("div");
+      square.classList.add("square");
+      square.style.width = `${squareSize}px`;
+      square.style.height = `${squareSize}px`;
+      container.appendChild(square);
 
+      square.addEventListener("mouseover", () => {
+        square.style.backgroundColor = "black";
+      });
+    }
+  }
+};
+
+const button = document.querySelector(".btn");
+button.addEventListener("click", () => {
+  let num = parseInt(prompt("Enter the number of squares per side: "), 10);
+
+  if (!num || num < 1 || num > 100) {
+    alert("Please enter a number between 1 and 100");
+    return;
+  }
+  createNewGrid(num);
+});
+
+createNewGrid(16);
